5260f990e628933c595a40924afdd21a790c2554
Gubar Sergey <serhii.hubar@nure.ua>
2017-05-21 23:27:33 +0300

    1-3 Tasks are done

--- Lab5ALGS/Program.cs
+++ Lab5ALGS/Program.cs
@@ -11,7 +11,7 @@ namespace Lab5ALGS
         static void Main(string[] args)
         {
             #region LinkedSortedListTask
-
+            //1
             LinkedSortedList list = new LinkedSortedList();
             Console.WriteLine("First list: ");
             list.AddItem(4);
@@ -37,7 +37,7 @@ namespace Lab5ALGS
 
 
             #region UnsortedLinkedListTask
-
+            //2.Create a linked unsorted list, print it, sort it by bubble sort, print the result.
             LinkedUnsortedList list2 = new LinkedUnsortedList();
             list2.AddItem(4);
             list2.AddItem(8);
@@ -53,7 +53,62 @@ namespace Lab5ALGS
 
             #endregion
 
+            #region arrayTask
 
+            //3.Given an array of integer numbers.Its two adjacent sections(from index F1 to
+            //L1 and from index L1+1 to L2) are sorted in ascending order. Print the array.
+            //It is necessary to merge them in ascending order into positions from F1 to 
+            //L2 without using an extra array(only 1 extra variable is allowed).Print array after merging.
+
+
+            int[] arr = { -4, 6, 7, 8, 12, -10, 13, 14 };
+            PrintArray(arr);
+            MergeAscending(arr,0,4,arr.Length-1);
+            PrintArray(arr);
+            #endregion
+
+
+        }
+
+        static void MergeAscending(int[] arr, int F1, int L1, int L2)
+        {
+
+            //F1 - начало отсортиванной первой части
+            //L1 - конец первой отсортированной части
+            //L1 + 1 - начало второй
+            //L2 - конец второй
+            for (int i = L1 + 1; i <= L2; i++)
+            {
+                for (int j = F1; j <= L1; j++)
+                {
+                    if(arr[i] < arr[j])
+                    {
+                        
+                        int temp = arr[i];
+                        for (int k = L1; k >= j; k--)
+                        {
+                            arr[k + 1] = arr[k];
+                        }
+                        arr[j] = temp;
+                        F1++;
+                        L1++;
+                        i++;
+                    }
+
+                }
+            }     
+        }
+        
+
+        
+
+        static void PrintArray(int[] arr)
+        {
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
