b1001ffcfecaa2719ea87140b01a4bf513eb56f1
PublicStaticVoidSasha <sasha.dolganenko@gmail.com>
2017-05-22 18:04:29 +0300

    Add files via upload

---
+++ Task 11/TooManyNodesException.java
@@ -0,0 +1,9 @@
+package com.company.Exercise10_11_12;
+
+public class TooManyNodesException extends Exception {
+
+    public TooManyNodesException(String message) {
+        super(message);
+    }
+
+}
